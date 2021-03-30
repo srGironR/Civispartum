801ef046724c6415157aa351488cab5223b4b4e2
Jonathan <jonathan.giron@uao.edu.co>
2021-03-29 19:47:46 -0500

    Foro y avances es la otras interfaces

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -3,7 +3,23 @@ import "../Profile/profile.css"
 
 
 const Profile=()=>(
-    <div>
+    <div className="ProfileAll">
+        <div className="Profile-inPH">
+            <div className="Profile-info">
+
+            </div>
+            <div className="PrReArH">
+                <div className="Prof-ReAr">
+
+                </div>
+                <div className="Prof-His">
+
+                </div>
+            </div>
+            <div className="Friends">
+                
+            </div>
+        </div>
 
     </div>
 );
