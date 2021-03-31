de0c2d72a29fdd2af563c2ed0c4a92a418d1b0d0
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2021-03-30 19:00:38 -0500

    Se arreglo el el fetch de datos se abre mensaje de warning

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
-import './App.css';
-import React from 'react';
-import Pag1 from './components/pag1/pag1';
-import Login from './components/login/login';
-import Register from './components/registro/registro';
-import Mmain from './components/Dashboard/main';
-import Std from './components/stads/std.js'
-import Help from './components/help/help.js'
-import Forum from './components/Forum/forum.js'
-import Profile from './components/Profile/profile.js'
-import Video from './components/Video/video.js'
+import "./App.css";
+import React from "react";
+import Pag1 from "./components/pag1/pag1";
+import Login from "./components/login/login";
+import Register from "./components/registro/registro";
+import Mmain from "./components/Dashboard/main";
+import Std from "./components/stads/std.js";
+import Help from "./components/help/help.js";
+import Forum from "./components/Forum/forum.js";
+import Profile from "./components/Profile/profile.js";
+import Video from "./components/Video/video.js";
 
-import {BrowserRouter, Route}  from 'react-router-dom'
-
+import { BrowserRouter, Route } from "react-router-dom";
 
 function App() {
   return (
     <BrowserRouter>
-    <div className="App">     
-     <Route exact path="/" component={Pag1}/>
-     <Route path="/Registro" component={Register}/>
-     <Route path="/Login" component={Login}/>
-     <Route path="/Temas" component={Mmain}/> 
-     <Route path="/Estadisticas" component={Std}/>
-     <Route path="/Ayuda" component={Help}/> 
-     <Route path="/Foro" component={Forum}/>
-     <Route path="/Perfil" component={Profile}/>
-     <Route path="/Video" component={Video}/>
-
-    </div>
+      <div className="App">
+        <Route exact path="/" component={Pag1} />
+        <Route path="/Registro" component={Register} />
+        <Route path="/Login" component={Login} />
+        <Route path="/Temas" component={Mmain} />
+        <Route path="/Estadisticas" component={Std} />
+        <Route path="/Ayuda" component={Help} />
+        <Route path="/Foro" component={Forum} />
+        <Route path="/Perfil" component={Profile} />
+        <Route path="/Video" component={Video} />
+      </div>
     </BrowserRouter>
   );
 }
