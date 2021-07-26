b4dafe38d9efaa7ca9efe87cf3bf8a9cad208111
joseDmultimedia <jose_dav.garcia@uao.edu.co>
2021-07-26 18:32:27 -0500

    Se añaden estilos en componente videos

--- src/components/Video2/Questions/badv2p4.js
+++ src/components/Video2/Questions/badv2p4.js
@@ -3,6 +3,8 @@ import "../Questions/v2pq.css";
 import p1 from "../Assets/bad.mp4";
 import { withRouter } from "react-router-dom";
 import ReactPlayer from "react-player";
+import {Animated} from "react-animated-css";
+import imageInt from "../../pics/volver-flecha.svg";
 
 class BadFinal extends React.Component {
   changeThis() {
@@ -24,6 +26,29 @@ class BadFinal extends React.Component {
             onEnded={this.changeThis.bind(this)}
           />
         </div>
+        <Animated
+          animationIn="fadeIn"
+          animationOut="fadeOut"
+          isVisible={true}
+          animationInDelay="500"
+        >
+          <div className="o-reward-text">
+            <h4>
+              Inténtalo de nuevo {localStorage.getItem("NombreUsuario")}, En la próxima 
+              lo lograras
+            </h4>
+            <p>
+              Si requieres de mas información al principio del modulo encuentras contenido extra
+            </p>
+            <div>
+              <img
+                className="o-image-cele"
+                src={imageInt}
+                alt="Imagen de celebración"
+              ></img>
+            </div>
+          </div>
+        </Animated>
       </div>
     );
   }
